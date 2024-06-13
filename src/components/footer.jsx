6066c4a3376700b4c117e2ff01fb5ebda04819b1
ahmar-js <ahmar@rapidlabs.ai>
2024-06-14 00:48:19 +0500

    Remove: Newsletter submit type

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -49,7 +49,7 @@ function Footer() {
                             <div className="newsletter-title py-4">
                                 <h2 className='text-white capitalize text-2xl'>Join our Newsletter</h2>
                             </div>
-                            <form action="">
+                            <form>
                                 <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                                     <div className="relative w-full">
                                         <label htmlFor="email" className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
@@ -59,7 +59,7 @@ function Footer() {
                                         <input className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required="" />
                                     </div>
                                     <div>
-                                        <button type="submit" style={{ backgroundColor: "#4F3DFE" }} className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg  cursor-pointer sm:rounded-none sm:rounded-r-lg focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
+                                        <button type="button" style={{ backgroundColor: "#4F3DFE" }} className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg  cursor-pointer sm:rounded-none sm:rounded-r-lg focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
                                     </div>
                                 </div>
                                 <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">*  Will send you weekly updates for your better solutions.</div>
